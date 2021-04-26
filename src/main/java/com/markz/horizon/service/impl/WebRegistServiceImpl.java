38a2a20c27e498cccf7efcbe5e377c9a6c875d18
MarkZ37 <595223036@qq.com>
2021-04-26 20:28:51 +0800

    实现注册功能

--- src/main/java/com/markz/horizon/service/impl/WebRegistServiceImpl.java
+++ src/main/java/com/markz/horizon/service/impl/WebRegistServiceImpl.java
@@ -1,15 +1,18 @@
 package com.markz.horizon.service.impl;
 
+import com.markz.horizon.entity.Useraccount;
 import com.markz.horizon.entity.base.BaseResponse;
 
 import com.markz.horizon.entity.model.WebRegistModel;
 import com.markz.horizon.mapper.UseraccountMapper;
 import com.markz.horizon.service.WebRegistService;
+import com.markz.horizon.utils.IdCardUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.validation.constraints.NotNull;
+
 
 @Service
 @Transactional
@@ -27,17 +30,30 @@ public class WebRegistServiceImpl implements WebRegistService {
     public @NotNull BaseResponse webRegistService(@NotNull WebRegistModel webRegistModel){
         System.out.println(webRegistModel);
         BaseResponse baseResponse = new BaseResponse();
-        if (useraccountMapper.selectByPrimaryKey(webRegistModel.getAccount()) != null){
-            //账号存在
+        IdCardUtil idCardUtil = new IdCardUtil();
+        String verification= idCardUtil.IdentityCardVerification(webRegistModel.getIdNumber());
+        if (verification.equals("correct")){
+            //身份证号验证通过
+            if (useraccountMapper.selectByPrimaryKey(webRegistModel.getAccount()) != null){
+                //用户存在
+                baseResponse.setStatus(REGISTFAILEDSTATUS);
+                baseResponse.setMessage(ACCOUNTEXIST);
+            }else {
+                //用户不存在
+                Useraccount useraccount = new Useraccount();
+                useraccount.setUsername(webRegistModel.getAccount());
+                useraccount.setPassword(webRegistModel.getPassword());
+                useraccount.setNickname(webRegistModel.getNickname());
+                useraccount.setIdnumber(webRegistModel.getIdNumber());
+                useraccountMapper.insert(useraccount);
+                baseResponse.setStatus(REGISTOKSTATUS);
+                baseResponse.setMessage(REGISTOK);
 
-            System.out.println("账号存在");
-            baseResponse.setMessage(ACCOUNTEXIST);
+            }
+        }else {
+            //身份证验证不通过
             baseResponse.setStatus(REGISTFAILEDSTATUS);
-        }else{
-            //账号不存在
-            System.out.println("账号不存在");
-            baseResponse.setMessage(REGISTOK);
-            baseResponse.setStatus(REGISTOKSTATUS);
+            baseResponse.setMessage(verification);
         }
         return baseResponse;
     }
